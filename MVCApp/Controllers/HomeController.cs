4e9689b650dd4b9154b0e84dd8913ded95f46e0b
Fedorovskiy Roman Dmytrovich <likethisnick@gmail.com>
2019-02-06 11:02:12 +0200

    db linked to mvc working just fine

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLibrary.BusinessLogic;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
+using MVCApp.Models;
 
 namespace MVCApp.Controllers
 {
@@ -26,9 +29,47 @@ namespace MVCApp.Controllers
 			return View();
 		}
 
-		public ActionResult SingUp()
+		public ActionResult ViewEmployees()
+		{
+			ViewBag.Message = "Employees list.";
+
+			var data = LoadEmployees();
+			List<EmployeeModel> employees = new List<EmployeeModel>();
+
+			foreach (var row in data)
+			{
+				employees.Add(new EmployeeModel
+				{
+					EmployeeId = row.EmployeeId,
+					FirstName = row.FirstName,
+					LastName = row.LastName,
+					EmailAddress = row.EmailAddress,
+					ConfirmEmail = row.EmailAddress
+				});
+			}
+
+			return View(employees);
+		}
+
+		public ActionResult SignUp()
 		{
 			ViewBag.Message = "Employee Sign Up";
+
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult SignUp(EmployeeModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				int recordsCreated = CreateEmployee(model.EmployeeId,
+					model.FirstName,
+					model.LastName,
+					model.EmailAddress);
+				return RedirectToAction("Index");
+			}
 
 			return View();
 		}
